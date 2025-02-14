8981bebc1b58bbf599f16f73c40e58f412b9086a
aloush-dev <github@aloush.dev>
2025-02-14 22:30:22 +0000

    start menu changes

--- src/apps/msnMessenger/Main.tsx
+++ src/apps/msnMessenger/Main.tsx
@@ -1,26 +1,11 @@
-import useAppStore from "../../stores/useAppStore";
-import useWindowManager from "../../stores/useWindowManager";
-import { useEffect } from "react";
-
 export const MessengerMainWindow = () => {
-  const { currentApp } = useAppStore();
-  const createWindow = useWindowManager((state) => state.createWindow);
-  const windows = useWindowManager((state) => state.windows);
-
-  // Get the window ID for the current app
-  const currentWindow = windows.find(
-    (window) => window.appId === currentApp?.id
+  return (
+    <div className="">
+      <div></div>
+      <div>
+        <div>Online</div>
+        <div>Offline</div>
+      </div>
+    </div>
   );
-
-  useEffect(() => {
-    if (currentApp && !currentWindow) {
-      createWindow(currentApp.id, "MSN Messenger");
-    }
-  }, [currentApp, currentWindow, createWindow]);
-
-  if (!currentWindow) {
-    return <div>Loading...</div>;
-  }
-
-  return <div className="relative w-full h-full"></div>;
 };
--- src/components/taskbar/Taskbar.tsx
+++ src/components/taskbar/Taskbar.tsx
@@ -1,18 +1,19 @@
-import { StartButton } from "./StartButton";
-
+import start from "/public/images/start.png";
 import { TaskbarItem } from "./TaskbarItem";
 import { TaskbarShortcuts } from "./Shortcuts";
 import { SystemTray } from "./SystemTray";
 import useWindowManager from "../../stores/useWindowManager";
 import useAppStore from "../../stores/useAppStore";
+import useTaskbarStore from "../../stores/useTaskbarStore";
 
 export const Taskbar = () => {
   const { installedApps } = useAppStore();
-
+  const { toggleMenu } = useTaskbarStore();
   const { windows } = useWindowManager();
+
   return (
     <div className="h-8 bg-[linear-gradient(to_bottom,#0058ee_0%,#3593ff_4%,#288eff_6%,#127dff_8%,#036ffc_10%,#0262ee_14%,#0057e5_20%,#0054e3_24%,#0055eb_56%,#005bf5_66%,#026afe_76%,#0062ef_86%,#0052d6_92%,#0040ab_94%,#003092_100%)] fixed bottom-0 w-full z-30 flex items-center shadow-md justify-between">
-      <StartButton />
+      <img onClick={toggleMenu} src={start} />
       <TaskbarShortcuts />
       <div className="flex-1 flex items-center">
         {windows.map((window) => {
@@ -25,7 +26,6 @@ export const Taskbar = () => {
           return <TaskbarItem key={window.id} app={app} windowId={window.id} />;
         })}
       </div>
-
       <SystemTray />
     </div>
   );
